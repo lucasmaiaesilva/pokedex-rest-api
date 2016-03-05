926a68d55a6ee5053dd5f18e37dac53796a80527
Lucas Maia e Silva <lucasmaiaesilva@gmail.com>
2016-03-04 23:28:14 -0300

    adding middleware to router

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ var express    = require('express');
 var app        = express();
 var bodyParser = require('body-parser');
 
+// requiring the model
+var Pokemon    = require('./models/pokemon');
+
+//connecting MongoDB with mongoose
+var mongoose   = require('mongoose');
+mongoose.connect('mongodb://localhost/pokemon', { poolSize: 1 }); // connect to our database
+
+var db = mongoose.connection;
+db.once('open', function(){
+	console.log('conected to MongoDB');
+});
+
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -10,9 +22,17 @@ app.use(bodyParser.json());
 var port = process.env.PORT || 8080;
 
 // ROUTES FOR OUR API
+// =============================================================================
 var router = express.Router();              // get an instance of the express Router
 
+// middleware to use for all requests
+router.use(function(req, res, next) {
+    // do logging
+    console.log('Something is happening.');
+    next(); // make sure we go to the next routes and don't stop here
+});
 
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });   
 });
@@ -22,6 +42,7 @@ router.get('/', function(req, res) {
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
 app.use('/api', router);
+
 
 // START THE SERVER
 app.listen(port);
