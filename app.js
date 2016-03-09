86bce683c1e632b9d436b20930be9ab71c41eb88
Lucas Maia e Silva <lucasmaiaesilva@gmail.com>
2016-03-09 09:13:20 -0300

    changing the sequence for separate files

--- app.js
+++ app.js
@@ -2,19 +2,20 @@ var express    = require('express');
 var app        = express();
 var bodyParser = require('body-parser');
 
-// requiring the model
-var Pokemon    = require('./models/pokemon');
-
-//connecting MongoDB with mongoose
-var mongoose   = require('mongoose');
-mongoose.connect('mongodb://localhost/pokemon', { poolSize: 1 }); // connect to our database
-
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;
+
+
+// requiring the model
+var Pokemon    = require('./models/pokemon');
+
+//connecting MongoDB with mongoose
+var mongoose   = require('mongoose');
+mongoose.connect('mongodb://localhost/pokemon', { poolSize: 1 }); // connect to our database
 
 // ROUTES FOR OUR API
 // =============================================================================
